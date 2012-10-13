855f6ee11bdea16dc7725b2f70c1569b05c398ea
Eric Priepke <epthegeek@gmail.com>
2012-10-12 19:45:44 -0500

    yarr

--- cc_modes/base.py
+++ cc_modes/base.py
@@ -200,7 +200,7 @@ class BaseGameMode(ep.EP_Mode):
     # to allow restarting the game
     def sw_startButton_active_for_2s(self,sw):
         # if there's a ball in the shooter lane and we're on a ball after ball 1
-        if self.switches.shooterLane.is_active() and self.game.ball > 1:
+        if self.game.switches.shooterLane.is_active() and self.game.ball > 1:
             self.game.game_reset()
 
     def sw_shooterLane_open_for_1s(self,sw):
