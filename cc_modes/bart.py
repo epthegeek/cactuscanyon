fec182578ed15ccbb5b01569993babdb4b79d22a
Eric Priepke <epthegeek@gmail.com>
2012-11-18 23:12:17 -0600

    tweaking flipper strength and bart wiggle

--- cc_modes/bart.py
+++ cc_modes/bart.py
@@ -391,18 +391,18 @@ class Bart(ep.EP_Mode):
         self.moving = True
         if version == 1:
             self.hat()
-            self.game.coils.moveBart.schedule(0x10010001,cycle_seconds=1)
-            self.game.coils.saloonFlasher.schedule(0x00000555,cycle_seconds=1)
+            self.game.coils.moveBart.schedule(0x00001001,cycle_seconds=1)
+            self.game.coils.saloonFlasher.schedule(0x00000333,cycle_seconds=1)
             self.delay(delay=0.15,handler=self.not_moving)
         # this one is for just talking - for the taunts
         if version == 2:
-            self.game.coils.moveBart.schedule(0x10010001,cycle_seconds=1)
+            self.game.coils.moveBart.schedule(0x00001001,cycle_seconds=1)
             self.game.coils.saloonFlasher.schedule(0x00000555,cycle_seconds=1)
             self.delay(delay=0.15,handler=self.not_moving)
         # this one is just the hat and the shaking with no light for bionic bart
         if version == 3:
             self.hat()
-            self.game.coils.moveBart.schedule(0x10010001,cycle_seconds=1)
+            self.game.coils.moveBart.schedule(0x00001001,cycle_seconds=1)
             self.delay(delay=0.15,handler=self.not_moving)
 
     def not_moving(self):
