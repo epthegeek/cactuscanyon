df53f3bfa4d90d5624eb006ace62d7639d75eaf4
Eric Priepke <epthegeek@gmail.com>
2013-05-24 10:25:15 -0500

    bug with the new tribute

--- game.py
+++ game.py
@@ -108,7 +108,7 @@ class CCGame(game.BasicGame):
         self.modes.modes = []
 
         # software version number
-        self.revision = "2013.05.23"
+        self.revision = "2013.05.24"
 
         # basic game reset stuff, copied in
 
