4bb3b7db3c46768924c374954e018fd039be4b77
Eric Priepke <epthegeek@gmail.com>
2013-07-24 11:40:49 -0500

    revision

--- game.py
+++ game.py
@@ -108,7 +108,7 @@ class CCGame(game.BasicGame):
         self.modes.modes = []
 
         # software version number
-        self.revision = "2013.07.23"
+        self.revision = "2013.07.24"
 
         # basic game reset stuff, copied in
 
