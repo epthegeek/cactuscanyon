e90cbb60e8d67363ad6beb6369287c7b09cb0c28
Eric Priepke <epthegeek@gmail.com>
2013-09-05 08:50:28 -0500

    revision

--- game.py
+++ game.py
@@ -110,7 +110,7 @@ class CCGame(game.BasicGame):
         self.tournament = False
 
         # software version number
-        self.revision = "2013.09.03"
+        self.revision = "2013.09.05"
 
         # basic game reset stuff, copied in
 
