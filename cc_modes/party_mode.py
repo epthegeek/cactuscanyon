38e92f71af5714bdc3ba7bef92e89a0cc446e67e
Eric Priepke <epthegeek@gmail.com>
2014-02-22 10:02:26 -0600

    attract display fix for various modes

--- cc_modes/party_mode.py
+++ cc_modes/party_mode.py
@@ -48,12 +48,14 @@ class PartyMode(ep.EP_Mode):
         elif self.game.party_setting == 'Drunk':
             textString1 = "< DRUNK - FLIPPERS REVERSED >"
         # last case is release to flip
+        elif self.game.party_setting == 'Newb':
+            textString1 = "< NEWB - DOUBLE FLIP >"
         else:
             textString1 = "< RELEASE TO FLIP >"
-            textLayer1 = ep.EP_TextLayer(64,0, self.game.assets.font_6px_az_inverse, "center", opaque = False).set_text(textString1,color=ep.MAGENTA)
-            script.append({'seconds':1.0,'layer':textLayer1})
-            # and another blank
-            script.append({'seconds':0.5,'layer':blank})
+        textLayer1 = ep.EP_TextLayer(64,0, self.game.assets.font_6px_az_inverse, "center", opaque = False).set_text(textString1,color=ep.MAGENTA)
+        script.append({'seconds':1.0,'layer':textLayer1})
+        # and another blank
+        script.append({'seconds':0.5,'layer':blank})
         # make a script layer with the two
         infoLine = dmd.ScriptedLayer(128,32,script)
         infoLine.composite_op = "blacksrc"
