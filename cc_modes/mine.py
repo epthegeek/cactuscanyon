813145a7168f24224e53e82b7ab380f4f2a074f4
Eric Priepke <epthegeek@gmail.com>
2014-08-15 11:01:38 -0500

    if stampede is running - mine just kicks out the ball

--- cc_modes/mine.py
+++ cc_modes/mine.py
@@ -56,7 +56,7 @@ class Mine(ep.EP_Mode):
             return
         # for MMB just kick out and last call
         if self.game.marshall_multiball.running or self.game.last_call.running or self.game.moonlight.running \
-            or self.game.showdown.running or self.game.ambush.running:
+            or self.game.showdown.running or self.game.ambush.running or self.stampede.running:
             self.game.mountain.eject()
             return
         # for bionic & cva - still collect extra ball
