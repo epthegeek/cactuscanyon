fe33c1e8554ad0126a120c4cf168dc32c8e5d2d6
Eric <epthegeek@gmail.com>
2015-06-04 17:52:04 -0500

    revison

--- game.py
+++ game.py
@@ -119,7 +119,7 @@ class CCGame(game.BasicGame):
         self.multiplier = 1
 
         # software version number
-        self.revision = "2015.05.22"
+        self.revision = "2015.06.04"
 
         # basic game reset stuff, copied in
         # load up the game data Game data
