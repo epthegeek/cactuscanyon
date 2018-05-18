139dcd21d8faae268e00b1eb7270ee117a1a7f78
Eric <epthegeek@gmail.com>
2018-05-18 11:45:03 -0500

    revision number change only

--- game.py
+++ game.py
@@ -119,7 +119,7 @@ class CCGame(game.BasicGame):
         self.multiplier = 1
 
         # software version number
-        self.revision = "2017.05.06"
+        self.revision = "2017.05.18"
 
         # basic game reset stuff, copied in
         # load up the game data Game data
