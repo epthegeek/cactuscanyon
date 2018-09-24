62d569c759731fa5dd13f3695ac883230e9e5df4
Eric <epthegeek@gmail.com>
2018-09-24 08:49:31 -0500

    upped the shot multiplier timer to 3 seconds in polly modes and added the new variants for startup quotes

--- game.py
+++ game.py
@@ -119,7 +119,7 @@ class CCGame(game.BasicGame):
         self.multiplier = 1
 
         # software version number
-        self.revision = "2017.05.18"
+        self.revision = "2018.09.24"
 
         # basic game reset stuff, copied in
         # load up the game data Game data
