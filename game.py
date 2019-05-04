2a55a0fe09cbfb451d3574893bc768e964a4c2bf
Eric <epthegeek@gmail.com>
2019-05-04 08:59:39 -0500

    changed bad lamp update call in the end of stampede

--- game.py
+++ game.py
@@ -119,7 +119,7 @@ class CCGame(game.BasicGame):
         self.multiplier = 1
 
         # software version number
-        self.revision = "2019.03.06"
+        self.revision = "2019.05.04"
 
         # basic game reset stuff, copied in
         # load up the game data Game data
